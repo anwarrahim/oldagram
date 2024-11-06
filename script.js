dc2328a2bab1cfdfef1a3a6bd6381fbaa9399017
Anwar Rahim <anwargithub@gmail.com>
2024-11-06 21:35:34 +0500

    post content

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ const postProfileAddress = document.getElementById("post-user-address")
 const userPostCaption = document.getElementById("user-post-caption")
 
 //user constructor function..... this create and initialize object.
-function userInfo(name, address, caption){
+function userInfo(name, address, caption, captionPerson){
     this.name = name
     this.address = address
     this.caption = caption
+    this.captionPerson =captionPerson
     
 }
 
 // create the object
 
-let user1 = new userInfo("Vincent van Gogh", "Zudert, Netherlands", "vincey1853 just took a few mushrooms lol")
+let user1 = new userInfo("Vincent van Gogh", "Zudert, Netherlands", "just took a few mushrooms lol", "vincey1853")
 postProfileName.innerHTML = `<p> ${user1.name} </p>`
 postProfileAddress.innerHTML = `<p> ${user1.address} </p>`
-userPostCaption.innerHTML = `<p> ${user1.caption}</p>`
+userPostCaption.innerHTML = `<p><span class="user-post-caption-person">${user1.captionPerson}</span> ${user1.caption}</p>`
